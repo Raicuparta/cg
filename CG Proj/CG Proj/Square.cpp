eb0d8712e34caac2a3c900810afb00e97530c344
Raicuparta <raicuparta@gmail.com>
2014-10-02 16:46:25 +0100

    Acrescentados objectos das superfícies (chão, rio)

--- CG Proj/CG Proj/Square.cpp
+++ CG Proj/CG Proj/Square.cpp
@@ -5,61 +5,39 @@ void myDisplay(void) {
 
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-	/*
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(0.0f, -1.0f, 0.0f);
-	glVertex3f(1.0f, -1.0f, 0.0f);
-	glVertex3f(1.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glEnd();
 
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glBegin(GL_POLYGON);
-	glVertex3f(-1.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, 1.0f, 0.0f);
-	glVertex3f(0.0f, -1.0f, 0.0f);
-	glVertex3f(-1.0f, -1.0f, 0.0f);
-	glEnd();
-	*/
-
-	/*glPushMatrix();
-	glColor3f(0.0f, 1.0f, 0.0f);
-	glTranslatef(0.0f, 0.0f, 2.0f);
-	
+	// calcada
+	glColor3f(0.4f, 0.1f, 0.7f);
 	glPushMatrix();
-	glTranslatef(2.1f, 0.0f, 0.0f);
-	
-	glPushMatrix();
-	glScalef(1.2f, 0.2f, 0.2f);
-	glutSolidCube(1);
+		glTranslatef(0.f, -6.f, 0.f);
+		glScalef(15.f, 1.f, 1.f);
+		glutSolidCube(1);
+		glTranslatef(0.f, 6.f, 0.f);
+		glutSolidCube(1);
 	glPopMatrix();
 
-	glTranslatef(0.5f, 0.1f, 0.0f);
-	glScalef(0.1f, 0.1f, 0.1f);
-	glutSolidCube(1);
-	glPopMatrix;
-	
-	glScalef(1.5f, 1.5f, 1.5f);
-	glutSolidCube(1);
-	glPopMatrix();*/
-
-	glPushMatrix();
-	glTranslatef(2.1f, 0.0f, 0.0f); 
-	glScalef(0.2f, 0.4f, 0.4f);
-
-	glPushMatrix();
-	glColor3f(1.0f, 0.5f, 0.0f);
-	glScalef(0.4f, 0.4f, 0.4f);
-	glutSolidCube(4);
+	// estrada
+	glColor3f(0.1f, 0.1f, 0.1f);
+		glPushMatrix();
+		glTranslatef(0.f, -3.f, 0.f);
+		glScalef(15.f, 5.f, 1.f);
+		glutSolidCube(1);
 	glPopMatrix();
 
-	glPushMatrix();
-	glTranslatef(-10.f, 4.f, 0.0f);
-	glColor3f(0.1f, 0.5f, 0.9f);
-	glScalef(40.f, 2.0f, 0.2f);
-	glutSolidCube(1);
+	// estrada
+	glColor3f(0.1f, 0.3f, 0.9f);
+		glPushMatrix();
+		glTranslatef(0.f, 3.f, 0.f);
+		glScalef(15.f, 5.f, 1.f);
+		glutSolidCube(1);
 	glPopMatrix();
+
+	// outro lado do rio
+	glColor3f(0.3f, 0.7f, 0.1f);
+		glPushMatrix();
+		glTranslatef(0.f, 6.5f, 0.f);
+		glScalef(15.f, 2.f, 1.f);
+		glutSolidCube(1);
 	glPopMatrix();
 
 	glFlush();
@@ -67,7 +45,11 @@ void myDisplay(void) {
 
 void myReshape(GLsizei width, GLsizei height) {
 
-	float xmin = -2., xmax = 2., ymin = -2., ymax = 2.;
+	// O ecra tem as dimensoes 15x15
+	// Cada unidade corresponde a um 'bloco' do jogo
+	// Por exemplo, a estrada tem uma largura de 5 unidades
+	float xmin = -7.5, xmax = 7.5, ymin = -7.5, ymax = 7.5;
+
 	float ratio = (xmax - xmin) / (ymax - ymin);
 	float aspect = (float) width / height; 
 	glViewport( 0, 0, width, height);
